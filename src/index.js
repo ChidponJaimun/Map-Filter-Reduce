49a68a9de4130c034af7598d7ccd658d9843e872
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-25 17:02:45 +0700

    2

--- src/index.js
+++ src/index.js
@@ -8,15 +8,28 @@ const newNumber = numbers.map(function (x) {
 console.log(newNumber);
 
 //Filter - Create a new array by keeping the items that return true.
-var numbers2 = [3, 56, 2, 48, 5];
-numbers2.filter(function (num) {
-  return num > 10;
+
+const newNumbers2 = numbers.filter(function (num) {
+  return num < 10;
 });
 
-console.log(numbers2);
+console.log(newNumbers2);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-
+const newNumbers3 = numbers.reduce(function (acc, currentNumber) {
+  return acc + currentNumber;
+});
+console.log(newNumbers3);
 //Find - find the first item that matches from an array.
 
+const newNumbers4 = numbers.find(function (x) {
+  return x > 4;
+});
+console.log(newNumbers4);
+
 //FindIndex - find the index of the first item that matches.
+const newNumbers5 = numbers.findIndex(function (x) {
+  // get index position
+  return x > 4;
+});
+console.log(newNumbers5);
